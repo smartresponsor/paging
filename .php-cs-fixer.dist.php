4e7eb38b071d0f9c0f0c2225f41479823a5ed8ff
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-07-09 12:48:22 -0500

    Exclude generated reference from fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 $finder = PhpCsFixer\Finder::create()
     ->in([__DIR__.'/src', __DIR__.'/tests'])
-    ->name('*.php');
+    ->name('*.php')
+    ->notPath('config/reference.php');
 
 return new PhpCsFixer\Config()
     ->setRiskyAllowed(true)
